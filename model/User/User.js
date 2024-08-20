a34cfa851cd4cd4c7a9fb06e605aa81907630b08
mohammed454545 <mohammed.h.salh@gmail.com>
2024-08-20 09:09:23 +0300

    User model

---
+++ model/User/User.js
@@ -0,0 +1,89 @@
+const mongoose=require ('mongoose')
+//schema
+const userSchema=new mongoose.Schema(
+  {
+    username:{
+    type:String,
+    require:true,
+  },
+  email:{
+    type:String,
+    require:true,
+  },
+  username:{
+    type:String,
+    require:true,
+  },
+  role:{
+    type:String,
+    require:true,
+    enum:["user","admin"],
+    default:'user',
+  },
+  password:{
+    type:String,
+    require:true,
+  },
+  lastLogin:{
+    type:Date,
+    default:Date.now('')
+  },
+  isVerified:{
+    type:String,
+    default:false
+  },
+  AccountLevel:{
+    type:String,
+    require:true,
+    enum:["bronze","silver","gold"],
+    default:'bronze'
+  },
+  profilePicture:{
+    type:String,
+    default:'',
+  },
+  coverImage:{
+    type:String,
+    default:'',
+  },
+  bio:{
+    type:String,
+
+  },
+  location:{
+    type:String,
+  },
+  NotificationPreferences:{
+    email:{type:String,default:true},
+    //..other notifications (sms)
+  },
+  location:{
+    type:String,
+    enum:['male','femaie']
+  },
+  profileViewers:[{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
+  followers:[{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
+  blokedUsers:[{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
+  posts:[{type:mongoose.Schema.Types.ObjectId,ref:"Post"}],
+  likedPosts:[{type:mongoose.Schema.Types.ObjectId,ref:"Post"}],
+  passwordResetToken:{
+    type:String,
+  },
+  passwordResetExpires:{
+    type:Date,
+  },
+  accountVerificationToken:{
+    type:String,
+  },
+  accountVerificationExpires:{
+    type:Date,
+  },
+},
+{
+  timestamps:true,
+}
+)
+
+//compile schema to model
+const User =mongoose.model("User",userSchema);
+module.exports=User;
